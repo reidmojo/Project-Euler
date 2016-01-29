163ff442c6ffc399b3c23a9293f5ebbcbe116c39
reidmojo <reidmjohnson@gmail.com>
2016-01-29 14:18:06 -0800

    prob 6 down

--- Problem 5.py
+++ Problem 5.py
@@ -6,4 +6,14 @@ Created on Fri Jan 29 12:57:58 2016
 
 @author: rjohnson26
 """
+j = []
+i = 20
 
+while len(j) == 0:
+    # hulk smash this problem
+    if i % 5 == 0 and i % 4 == 0 and i % 3 == 0 and i % 2 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0 and i % 11 == 0 and i % 12 == 0 and i % 13 == 0 and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0 and i % 20 == 0:
+    # don't judge me it works. hulk sma
+        j.append(i)
+    else:
+        i=i+1
+print j
