f898b557fa15a6535f1f8750b7be374d06c86927
reidmojo <reidmjohnson@gmail.com>
2016-01-29 11:22:22 -0800

    Prob 3 solved

--- Problem 3.py
+++ Problem 3.py
@@ -12,26 +12,26 @@ Created on Thu Jan 28 18:16:09 2016
 #Factors of 600851475143
 prime_list  = []
 factor_list = []
-factoring_agent = 600851475143
+f_num = 600851475143
     
 #is it a factor?        
-for i in range(1,factoring_agent):    
-    if factoring_agent % i == 0:
+for i in range(1,int(f_num**0.5)+1):    
+    if f_num % i == 0:
+        n = f_num // i
         factor_list.append(i)
-    else:
-        pass
-
+        factor_list.append(n)
+    
+    
 print 'done factoring!'
 #doing factors is easier than primes, so do it first
 
 #is prime?
 for i in factor_list:
     templist = []
-    for n in range (2,i):
+    for n in range (2,int(i**0.5)+1):
         if i % n == 0:
-            templist.append(i)
-        else:
-            pass
+            templist.append(n)
+            templist.append(i//n)
     if len(templist) == 0:
         prime_list.append(i)
         
