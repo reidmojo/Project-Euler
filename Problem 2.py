cb18f61f225fa7dfd9b805ca8f82ade26a565f41
Reid Johnson <reid@Reids-MacBook-Pro.local>
2016-01-28 18:03:52 -0800

    prob 2 down

--- Problem 2.py
+++ Problem 2.py
@@ -8,3 +8,18 @@ Created on Thu Jan 28 17:36:43 2016
 @author: reid
 """
 
+fib = []
+a = 0
+b = 1
+c=1
+
+while c <= 4000000:
+
+    c = a+b
+    if c % 2 == 0:
+        fib.append(c)
+    a = b
+    b = c
+    
+print sum(fib) 
+    
