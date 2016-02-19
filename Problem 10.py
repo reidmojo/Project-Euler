dce4501f4432ad67a9fe36eec7421042f6ecce8b
reidmojo <reidmjohnson@gmail.com>
2016-02-18 16:58:05 -0800

    Working on P11

--- Problem 10.py
+++ Problem 10.py
@@ -9,3 +9,32 @@ Created on Fri Jan 29 15:08:17 2016
 @author: rjohnson26
 """
 
+def is_prime(n):
+    templist = []
+    for i in range(1,int(round((n**0.5)+1,0))):
+        if n%i == 0:
+            templist.append(i)
+        else:
+            pass
+    if len(templist) == 1:
+        return 1
+        
+def listsum(l):
+    s = 0    
+    for i in l:
+        s = s+ i
+    return s
+
+primelist = []
+
+for i in range(2,2000000):
+    if is_prime(i) == 1:
+        primelist.append(i)
+    if i%10000 == 0:
+        print i
+    else:
+        pass
+
+print listsum(primelist)
+#        if 4%i == 0:
+#            templist.append(i)
